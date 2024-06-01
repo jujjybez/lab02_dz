a429c060f6114a13ea100cade8b85436501e3e94
jujjybez <144797201+jujjybez@users.noreply.github.com>
2024-06-01 17:30:05 +0300

    added comments to hello_world.cpp

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main() {
-std::string name;
-std::cout << "Enter name: "; 
-std::cin >> name;
-std::cout << std::endl << "Hello world from " << name;
+std::string name; // create string 
+std::cout << "Enter name: "; // a message to enter a name 
+std::cin >> name; // enter a name from a keyboard 
+std::cout << std::endl << "Hello world from " << name; // display the message with a name 
 return 0;
 }
