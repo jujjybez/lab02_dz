c5aed749779c7a53a4c8cffb4b4a53f0beb4f098
jujjybez <144797201+jujjybez@users.noreply.github.com>
2024-06-01 17:42:26 +0300

    changed code-style

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
-int main() {
-std::string name; // create string 
-std::cout << "Enter name: "; // a message to enter a name 
-std::cin >> name; // enter a name from a keyboard 
-std::cout << std::endl << "Hello world from " << name; // display the message with a name 
-return 0;
+int
+main()
+{
+  std::string name;            // create string
+  std::cout << "Enter name: "; // a message to enter a name
+  std::cin >> name;            // enter a name from a keyboard
+  std::cout << std::endl
+            << "Hello world from " << name; // display the message with a name
+  return 0;
 }
